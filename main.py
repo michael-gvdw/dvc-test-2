222896aa464f881302dd45c8e677ac5b39529050
Michael Groenewegen van der Weijden <michael@fragrant-chios.com>
2021-03-31 11:07:04 +0200

    initial dvc with datasets

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-
+import pandas as pd
 
 
 if __name__ == '__main__':
-    print('Hello World!')
+    df = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/car/car.data', header=None)
+    df.to_csv('./assets/original_dataset/car_eval_raw.csv', index=False, mode='w')
+
+    df.columns = ['buying', 'maint', 'doors', 'persons', 'lug_boot', 'safety', 'class']
+    df.to_csv('./assets/data/car_eval.csv', index=False, mode='w')
+
+
+
